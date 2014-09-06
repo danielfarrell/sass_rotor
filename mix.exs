8f7c048ba811cce228ec2fb6f528522f8c34626e
Andrew Bredow <bredow@gmail.com>
2014-09-06 15:28:41 -0400

    Bump version to work with rc1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SassRotor.Mixfile do
   def project do
     [app: :sass_rotor,
      version: "0.0.1",
-     elixir: "~> 0.14.2",
+     elixir: "~> 1.0.0",
      description: description,
      package: package,
      deps: deps]
